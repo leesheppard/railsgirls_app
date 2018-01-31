7ea515f431fbc3aee8e3809f1212e61eaf1ebf78
Lee Sheppard <lee@leesheppard.com>
2018-01-31 22:28:21 +1100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,11 @@
 ActiveRecord::Schema.define(version: 20170528043939) do
 
   create_table "ideas", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "picture"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "name"
+    t.text "description"
+    t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
